7f9bb414663175e17cf7409c58c4bad173706ce1
plries <apaulries@gmail.com>
2024-08-07 01:31:44 -0700

    finished sections, 768 breakpoint

--- js/faq.js
+++ js/faq.js
@@ -1,31 +1,16 @@
 const faqs = document.querySelectorAll('.faq-item');
 
 function toggleFaq() {
+    
+    
+    faqs.forEach(el => {
+        if (el !== this) {
+            el.classList.remove('expand');
+        }
+    })
 
-    faqs.forEach(faq => {
-        faq.style.maxHeight = "5.8rem";
-        const btn = faq.querySelector('button');
-        const caret = btn.querySelector('ion-icon');
-        if (caret) {
-            caret.style.transform = 'rotate(0deg)';
-        }
-    });
+    this.classList.toggle('expand');
 
-    if (this.style.maxHeight == '5.8rem') {
-        this.style.maxHeight = "1000px";
-        const btn = this.querySelector('button');
-        const caret = btn.querySelector('ion-icon');
-        if (caret) {
-            caret.style.transform = 'rotate(180deg)';
-        }
-    } else {
-        this.style.maxHeight = '5.8rem';
-        const btn = this.querySelector('button');
-        const caret = btn.querySelector('ion-icon');
-        if (caret) {
-            caret.style.transform = 'rotate(0deg)';
-        }
-    }
 }
 
 faqs.forEach(faq => faq.addEventListener('click', toggleFaq));
